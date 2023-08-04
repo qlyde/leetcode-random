dc506d092f678cbb616f586cb57698015ea11842
James Kroeger <jameskroeger123@gmail.com>
2023-08-04 22:34:44 +1000

    Time: 54 ms (83.3%), Space: 16.4 MB (24.26%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cols = set()
+        diags = set()
+        rdiags = set()
+
+        def canPlace(row, col):
+            if col in cols or row + col in diags or col - row in rdiags:
+                return False
+            return True
+
+        def place(row, col):
+            cols.add(col)
+            diags.add(row + col)
+            rdiags.add(col - row)
+
+        def remove(row, col):
+            cols.remove(col)
+            diags.remove(row + col)
+            rdiags.remove(col - row)
+
+        ret = 0
+        def backtrack(row):
+            nonlocal ret
+            for col in range(n):
+                if canPlace(row, col):
+                    if row == n - 1:
+                        ret += 1
+                    place(row, col)
+                    backtrack(row + 1)
+                    remove(row, col)
+
+        backtrack(0)
+        return ret
