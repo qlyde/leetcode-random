65c534df87636e98bf6266e8bef686f8cc0bbb87
James Kroeger <jameskroeger123@gmail.com>
2023-08-06 07:29:26 +1000

    Time: 50 ms (74.23%), Space: 16.4 MB (26.84%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -2,20 +2,6 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         if nums == [0] * len(nums):
             return "0"
-
         def cmp(a, b):
-            ai = bi = 0
-            while ai < len(a) and bi < len(b):
-                if int(a[ai]) > int(b[bi]):
-                    return -1
-                elif int(a[ai]) < int(b[bi]):
-                    return 1
-                else:
-                    ai += 1
-                    bi += 1
-            if ai < len(a):
-                return cmp(a[ai:], b)
-            if bi < len(b):
-                return cmp(a, b[bi:])
-            return 0
-        return "".join(sorted([str(num) for num in nums], key=cmp_to_key(cmp)))
+            return -1 if a + b < b + a else (1 if a + b > b + a else 0)
+        return "".join(sorted([str(num) for num in nums], key=cmp_to_key(cmp), reverse=True))
