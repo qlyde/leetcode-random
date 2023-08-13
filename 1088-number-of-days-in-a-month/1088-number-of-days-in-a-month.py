0f50015d9bb6171445d910d5481b41aa1bc5799d
qlyde <jameskroeger123@gmail.com>
2023-08-14 05:06:52 +1000

    Time: 42 ms (67.57%), Space: 16.3 MB (74.32%) - LeetHub

--- 1088-number-of-days-in-a-month/1088-number-of-days-in-a-month.py
+++ 1088-number-of-days-in-a-month/1088-number-of-days-in-a-month.py
@@ -1,15 +1,7 @@
 class Solution:
     def numberOfDays(self, year: int, month: int) -> int:
-      thirtyOne = set([1, 3, 5, 7, 8, 10, 12])
-      thirty = set([4, 6, 9, 11])
-      if month in thirtyOne:
-        return 31
-      if month in thirty:
-        return 30
-      if year % 4 != 0:
-        return 28
-      if year % 100 != 0:
-        return 29
-      if year % 400 == 0:
-        return 29
-      return 28
+      days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      ret = days[month - 1]
+      if month == 2 and ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+        return ret + 1
+      return ret
