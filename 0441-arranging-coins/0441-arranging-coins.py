38b8fc7e28650c10fd510e4e166413144112ea25
qlyde <jameskroeger123@gmail.com>
2023-08-15 05:52:03 +1000

    Time: 41 ms (92.11%), Space: 16.2 MB (90.4%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,7 +1,11 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-      row = 1
-      while n >= row:
-        n -= row
-        row += 1
-      return row - 1
+      # 1 + 2 + ... + k = k(k+1)/2
+      # max k such that k(k+1)/2 <= N
+      #
+      # k(k+1) <= 2N
+      # k^2 + k <= 2N
+      # (k + 1/2)^2 - 1/4 <= 2N
+      # (k + 1/2)^2 <= 2N + 1/4
+      # k = floor(sqrt(2N + 1/4) - 1/2)
+      return (int)((2 * n + 0.25) ** 0.5 - 0.5)
